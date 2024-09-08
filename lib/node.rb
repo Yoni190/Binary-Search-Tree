02fc6b36265aad6425e90215642b6cde3f443ec2
Yoni190 <yfisuhu@gmail.com>
2024-09-08 16:42:54 +0300

    Create a method to insert a leaf node

--- lib/node.rb
+++ lib/node.rb
@@ -3,6 +3,8 @@ class Node
 
   def initialize(data)
     self.data = data
+    self.left = nil
+    self.right = nil
   end
 
 end
--- lib/tree.rb
+++ lib/tree.rb
@@ -12,7 +12,7 @@ class Tree
     end_index = sorted_array.length - 1
     mid = sorted_array.length / 2
     if sorted_array[start_index] == sorted_array[end_index]
-      return nil
+      return Node.new(sorted_array[start_index])
     end
 
     root = Node.new(sorted_array[mid])
@@ -20,6 +20,25 @@ class Tree
     root.right = build_tree(sorted_array[mid + 1..end_index])
 
     root
+  end
+
+  def insert(value, root = @root)
+    if value < root.data
+      left_data = root.left
+      if left_data.data.nil?
+        root.left = Node.new(value)
+        return "Node added"
+      end
+      insert(value, left_data)
+    else
+      right_data = root.right
+      if right_data.data.nil?
+        root.right = Node.new(value)
+        return "Node Added!"
+      end
+      
+      insert(value, right_data)
+    end
   end
 
   def pretty_print(node = @root, prefix = '', is_left = true)
@@ -31,4 +50,6 @@ class Tree
 end
 
 t1 = Tree.new([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
+t1.pretty_print
+puts t1.insert(30)
 t1.pretty_print
