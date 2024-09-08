7fa425782471270bbbb88dccdd1f9d31bc9f3e94
Yoni190 <yfisuhu@gmail.com>
2024-09-08 15:53:09 +0300

    Add #pretty_print method

--- lib/tree.rb
+++ lib/tree.rb
@@ -1,5 +1,6 @@
 require_relative "node"
 class Tree
+  attr_accessor :root
   def initialize(array)
     @root = build_tree(array)
   end
@@ -15,11 +16,19 @@ class Tree
     end
 
     root = Node.new(sorted_array[mid])
-    root.left = Node.new(build_tree(sorted_array[start_index..mid - 1]))
-    root.right = Node.new(build_tree(sorted_array[mid + 1..end_index]))
+    root.left = build_tree(sorted_array[start_index..mid - 1])
+    root.right = build_tree(sorted_array[mid + 1..end_index])
 
     root
   end
+
+  def pretty_print(node = @root, prefix = '', is_left = true)
+    pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
+    puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
+    pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
+  end
+ 
 end
 
-t1 = Tree.new([1,2,3,4,5,6,7,8,9])
+t1 = Tree.new([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
+t1.pretty_print
